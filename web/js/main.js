1cf8ba9c08427ea80951faf5020402a7520bf4fd
Grzegorz Kaszuba <uirapuruadg@gmail.com>
2013-10-23 11:47:38 +0200

    symfony installed

---
+++ web/js/main.js
@@ -0,0 +1,9 @@
+//function ExpCtrl($scope) {
+//    $scope.noIt = false;
+////
+////    $scope.show = function() {
+////        $scope.noIt = true;
+////        console.log("click");
+////        console.log($scope.noIt);
+////    };
+//}
